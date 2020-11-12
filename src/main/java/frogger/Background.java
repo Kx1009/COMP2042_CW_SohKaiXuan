a9578d76aabc6a6e71c6845536cf9ab1392d84da
Kaixuan1009 <xuankaisoh@gmail.com>
2020-11-12 19:32:38 +0800

    Move Background out from Main

--- src/main/java/frogger/Background.java
+++ src/main/java/frogger/Background.java
@@ -2,11 +2,54 @@ package frogger;
 
 public class Background extends World {
 
+    private int Endx;
+    private int Endy;
     public void act(long now) {
 
     }
 
     public Background(){
+        BackgroundImage froggerback = new BackgroundImage();
+        this.add(froggerback);
+
+        this.add(new Log(2, 150, 0, 166, 0.75));
+        this.add(new Log(2, 150, 220, 166, 0.75));
+        this.add(new Log(2, 150, 440, 166, 0.75));
+
+        this.add(new Log(0, 300, 0, 276, -2));
+        this.add(new Log(0, 300, 400, 276, -2));
+
+        this.add(new Log(2, 150, 50, 329, 0.75));
+        this.add(new Log(2, 150, 270, 329, 0.75));
+        this.add(new Log(2, 150, 490, 329, 0.75));
+
+
+        this.add(new Turtle(500, 376, -1, 130, 130));
+        this.add(new Turtle(300, 376, -1, 130, 130));
+        this.add(new WetTurtle(700, 376, -1, 130, 130));
+        this.add(new WetTurtle(600, 217, -1, 130, 130));
+        this.add(new WetTurtle(400, 217, -1, 130, 130));
+        this.add(new WetTurtle(200, 217, -1, 130, 130));
+
+        Endx = 141-13;
+        Endy = 96;
+        for (int i = 0; i < 5; i++) {
+            this.add(new End(13+i*Endx,Endy));
+        }
+
+        this.add(new Obstacle(3, 0, 649, 1, 120, 120));
+        this.add(new Obstacle(3, 300, 649, 1, 120, 120));
+        this.add(new Obstacle(3, 600, 649, 1, 120, 120));
+
+        this.add(new Obstacle(0, 100, 597, -1, 50, 50));
+        this.add(new Obstacle(0, 250, 597, -1, 50, 50));
+        this.add(new Obstacle(0, 400, 597, -1, 50, 50));
+        this.add(new Obstacle(0, 550, 597, -1, 50, 50));
+        this.add(new Obstacle(5, 0, 540, 1, 200, 200));
+        this.add(new Obstacle(5, 500, 540, 1, 200, 200));
+        this.add(new Obstacle(0, 500, 490, -5, 50, 50));
+        // reposition the score
+        this.add(new Digit(0, 30, 565, 25));
 
     }
 }
--- src/main/java/frogger/Main.java
+++ src/main/java/frogger/Main.java
@@ -21,26 +21,20 @@ public class Main extends Application {
 	    background = new Background();
 	    music = new Music();
 	    Scene scene  = new Scene(background,600,800);
-	    
-		//Obstacle obstacle = new Obstacle("file:src/p4_group_8_repo/truck1Right.png", 25, 25, 3);
-		//Obstacle obstacle1 = new Obstacle("file:src/p4_group_8_repo/truck2Right.png", 100, 100,2 );
-		//Obstacle obstacle2 = new Obstacle("file:src/p4_group_8_repo/truck1Right.png",0,  150, 1);
-
+		/*
 		BackgroundImage froggerback = new BackgroundImage();
-	    
 		background.add(froggerback);
-
 		background.add(new Log(2, 150, 0, 166, 0.75));
 		background.add(new Log(2, 150, 220, 166, 0.75));
 		background.add(new Log(2, 150, 440, 166, 0.75));
-		//background.add(new Log("file:src/p4_group_8_repo/log3.png", 150, 0, 166, 0.75));
+
 		background.add(new Log(0, 300, 0, 276, -2));
 		background.add(new Log(0, 300, 400, 276, -2));
-		//background.add(new Log("file:src/p4_group_8_repo/log.png", 300, 800, 276, -2));
+
 		background.add(new Log(2, 150, 50, 329, 0.75));
 		background.add(new Log(2, 150, 270, 329, 0.75));
 		background.add(new Log(2, 150, 490, 329, 0.75));
-		//background.add(new Log("file:src/p4_group_8_repo/log3.png", 150, 570, 329, 0.75));
+
 
 		background.add(new Turtle(500, 376, -1, 130, 130));
 		background.add(new Turtle(300, 376, -1, 130, 130));
@@ -48,36 +42,17 @@ public class Main extends Application {
 		background.add(new WetTurtle(600, 217, -1, 130, 130));
 		background.add(new WetTurtle(400, 217, -1, 130, 130));
 		background.add(new WetTurtle(200, 217, -1, 130, 130));
-		//background.add(new Log("file:src/p4_group_8_repo/log2.png", 200, 100, 1));
-		//background.add(new Log("file:src/p4_group_8_repo/log2.png", 0, 100, 1));
-		//background.add(new Log("file:src/p4_group_8_repo/log2.png", 100, 120, -1));
-		//background.add(new Log("file:src/p4_group_8_repo/log2.png", 200, 120, -1));
-		//background.add(new Log("file:src/p4_group_8_repo/log2.png", 100, 140, 1));
-		//background.add(new Log("file:src/p4_group_8_repo/log2.png", 200, 140, 1));
-		//background.add(new Log("file:src/p4_group_8_repo/log2.png", 100, 160, -1));
-		//background.add(new Log("file:src/p4_group_8_repo/log2.png", 300, 160, -1));
-		//background.add(new Log("file:src/p4_group_8_repo/log2.png", 100, 180, 1));
-		//background.add(new Log("file:src/p4_group_8_repo/log2.png", 200, 180, 1));
-		//background.add(new Log("file:src/p4_group_8_repo/log2.png", 100, 200, -1));
-		//background.add(new Log("file:src/p4_group_8_repo/log2.png", 200, 200, -1));
-		//background.add(new Log("file:src/p4_group_8_repo/log2.png", 100, 220, 1));
-		//background.add(new Log("file:src/p4_group_8_repo/log2.png", 200, 220, 1));
-		//background.add(new Log("file:src/p4_group_8_repo/log2.png", 400, 220, 1));
-		//End end2 = new End();
-		//End end3 = new End();
-		//End end4 = new End();
-		//End end5 = new End();
+
 		background.add(new End(13,96));
 		background.add(new End(141,96));
 		background.add(new End(141 + 141-13,96));
 		background.add(new End(141 + 141-13+141-13+1,96));
 		background.add(new End(141 + 141-13+141-13+141-13+3,96));
-		animal = new Animal();
-		background.add(animal);
+
 		background.add(new Obstacle(3, 0, 649, 1, 120, 120));
 		background.add(new Obstacle(3, 300, 649, 1, 120, 120));
 		background.add(new Obstacle(3, 600, 649, 1, 120, 120));
-		//background.add(new Obstacle("file:src/p4_group_8_repo/truck1"+"Right.png", 720, 649, 1, 120, 120));
+
 		background.add(new Obstacle(0, 100, 597, -1, 50, 50));
 		background.add(new Obstacle(0, 250, 597, -1, 50, 50));
 		background.add(new Obstacle(0, 400, 597, -1, 50, 50));
@@ -87,9 +62,11 @@ public class Main extends Application {
 		background.add(new Obstacle(0, 500, 490, -5, 50, 50));
 		// reposition the score
 		background.add(new Digit(0, 30, 565, 25));
-		//background.add(obstacle);
-		//background.add(obstacle1);
-		//background.add(obstacle2);
+		*/
+
+		animal = new Animal();
+		background.add(animal);
+
 		background.start();
 		primaryStage.setScene(scene);
 		primaryStage.show();
